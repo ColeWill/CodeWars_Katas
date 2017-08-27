6e9b4439151e9337267bc2875250c2c06aa30de6
CoolCode2 <wcole2222@gmail.com>
2017-08-27 12:57:13 -0600

    narcissistic number checker completed

---
+++ narcissistic_number.js
@@ -0,0 +1,35 @@
+//narcissistic numbers
+
+// 153 = 1^3 + 5^3 + 3^3 = 1 + 125 +27 = 153
+
+//node doesnt care about "54" or 54 ...
+
+function narcissistic(value){
+	console.log('check that number!');
+	var num = value;
+	var numString = num.toString();
+	var length = numString.length;
+	console.log('length:', length);
+
+
+	var finalArray = [];
+	for (var i = 0; i < numString.length; i++) {
+		
+		finalArray.push(Math.pow(numString[i], length));
+
+	}
+	console.log(finalArray);
+
+	var final = finalArray.reduce(function(prev,cur){
+		return prev + cur; 
+		
+	});
+
+	console.log('final:', final);
+
+	if(final === value){
+		console.log(true);
+	} else{ console.log(false) }
+}	
+
+narcissistic(262144);
